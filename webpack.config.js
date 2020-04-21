6a93e26788c2790ff3d9aa819d27c0ab9fcb1d8d
Naseebullah Safi <javastastic@gmail.com>
2020-04-21 14:12:45 +0100

    feat: webpack bundle setup for dev and production

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path"),
+  webpack = require("webpack"),
+  webpackMerge = require("webpack-merge"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  ResourcesManifestPlugin = require("resources-manifest-webpack-plugin");
+
+/**
+ * Will correctly call the file based on the mode (i.e. prod/dev)
+ * @param {*} env - is the envrironment variable from the command
+ */
+function modeConfig(env) {
+  return require(`${path.resolve(__dirname)}/webpack.${env}`)(env);
+}
+
+module.exports = ({ mode } = { mode: "production" }) =>
+  webpackMerge(
+    {
+      mode,
+      entry: "./src/index.js",
+      optimization: {
+        splitChunks: {
+          chunks: "all",
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            use: {
+              loader: "babel-loader",
+            },
+            exclude: /node_modules/,
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg|webp)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "static/media/[name].[contenthash].[ext]",
+                },
+              },
+            ],
+          },
+        ],
+      },
+      resolve: {
+        extensions: ["*", ".js", ".jsx"],
+        alias: {
+          components: path.resolve(__dirname, "./src/components"),
+        },
+      },
+      devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, "public"),
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "public/index.html",
+          inject: "body",
+          minify: {
+            html5: true,
+            removeComments: true,
+            collapseWhitespace: true,
+          },
+          templateParameters: {
+            PUBLIC_URL: "",
+          },
+        }),
+        new ResourcesManifestPlugin(
+          {
+            TO_CACHE: /.+\.(js|css|png|jpe?g|gif|svg|webp)$/,
+          },
+          "public/service-worker.js"
+        ),
+        new webpack.ProgressPlugin(),
+      ],
+    },
+    modeConfig(mode)
+  );
---
+++ webpack.production.js
@@ -0,0 +1,38 @@
+const path = require("path"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin"),
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = () => ({
+  devtool: "none",
+  output: {
+    path: path.resolve(__dirname, "./build/"),
+    publicPath: "/",
+    filename: "static/js/[name].[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash].css",
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "public/",
+        to: ".",
+        ignore: ["service-worker.js"],
+      },
+    ]),
+    new OptimizeCSSAssetsPlugin(),
+  ],
+});
