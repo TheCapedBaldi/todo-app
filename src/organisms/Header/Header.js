e6915f21ecf4e1cd26f8bc9c7d30743c62370ba6
Naseebullah Safi <javastastic@gmail.com>
2020-04-21 22:18:19 +0100

    feat: create header component

---
+++ src/organisms/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { StyledHeader } from "./Header.style";
+import Navbar from "../Navbar";
+
+const Header = () => (
+  <StyledHeader>
+    <Navbar />
+  </StyledHeader>
+);
+
+export default Header;
---
+++ src/organisms/Header/Header.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  max-width: 800px;
+  width: 100%;
+`;
