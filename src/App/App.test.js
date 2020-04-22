273873548502373219e0402b4216a639c912f4ca
Naseebullah Safi <javastastic@gmail.com>
2020-04-22 19:17:18 +0100

    fix: test for App component

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,11 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 import "jest-styled-components";
 
+import { withReactRouter } from "src/utility/withReactRouter";
 import App from "./App";
 
 describe("App", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = withReactRouter(<App />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
