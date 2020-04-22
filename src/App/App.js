3ffdec6bc505512ab531d9aeb2c06ad16605690a
Naseebullah Safi <javastastic@gmail.com>
2020-04-22 13:41:15 +0100

    fix: refactor app component with new components

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+
+import { GlobalStyles } from "../global.style";
+import Header from "src/organisms/Header";
+import Main from "src/organisms/Main";
 import { StyledApp } from "./App.style";
 
-const App = () => <StyledApp>Hello World</StyledApp>;
+/**
+ * App component - is the top most root component
+ * which wraps everything. inc. global styles.
+ */
+const App = () => (
+  <StyledApp>
+    <GlobalStyles />
+    <Header />
+    <Main />
+  </StyledApp>
+);
 
 export default App;
--- src/App/App.style.js
+++ src/App/App.style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const StyledApp = styled.div`
-  background-color: red;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 `;
