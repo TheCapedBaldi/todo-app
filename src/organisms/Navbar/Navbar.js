6f039af725373de1ab0b2d3f12a2a1be122ee962
Naseebullah Safi <javastastic@gmail.com>
2020-04-22 13:45:13 +0100

    fix: refactor navbar component

--- src/organisms/Navbar/Navbar.js
+++ src/organisms/Navbar/Navbar.js
@@ -1,13 +1,65 @@
 import React from "react";
-
 import NavbarItem from "src/molecules/NavbarItem";
 import { StyledNavbar } from "./Navbar.style";
 
 const Navbar = () => {
   return (
     <StyledNavbar>
-      <NavbarItem to="/todos">Todos</NavbarItem>
-      <NavbarItem to="/create">Create</NavbarItem>
+      <NavbarItem to="/todos" activeClassName="selected">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-layers"
+        >
+          <polygon points="12 2 2 7 12 12 22 7 12 2" />
+          <polyline points="2 17 12 22 22 17" />
+          <polyline points="2 12 12 17 22 12" />
+        </svg>
+        Todos
+      </NavbarItem>
+      <NavbarItem to="/create" activeClassName="selected">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-plus-square"
+        >
+          <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+          <line x1="12" y1="8" x2="12" y2="16" />
+          <line x1="8" y1="12" x2="16" y2="12" />
+        </svg>
+        Create
+      </NavbarItem>
+      <NavbarItem to="/play" activeClassName="selected">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-play"
+        >
+          <polygon points="5 3 19 12 5 21 5 3" />
+        </svg>
+        Play
+      </NavbarItem>
     </StyledNavbar>
   );
 };
--- src/organisms/Navbar/Navbar.style.js
+++ src/organisms/Navbar/Navbar.style.js
@@ -3,8 +3,25 @@ import styled from "styled-components";
 export const StyledNavbar = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
   list-style: none;
   margin: 3em 0 5em;
   padding-left: 0;
+
+  @media screen and (min-width: 200px) {
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 400px) {
+    justify-content: space-between;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-right: 9px;
+    }
+  }
 `;
