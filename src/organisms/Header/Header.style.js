fa4b0320022b8d0f75b95f38668b6a8ab1c2f62c
Naseebullah Safi <javastastic@gmail.com>
2020-04-22 13:42:07 +0100

    fix: refactor header component to facilitate viewports

--- src/organisms/Header/Header.style.js
+++ src/organisms/Header/Header.style.js
@@ -1,6 +1,17 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  max-width: 800px;
   width: 100%;
+
+  @media screen and (min-width: 300px) {
+    max-width: 300px;
+  }
+
+  @media screen and (min-width: 400px) {
+    max-width: 400px;
+  }
+
+  @media screen and (min-width: 600px) {
+    max-width: 600px;
+  }
 `;
