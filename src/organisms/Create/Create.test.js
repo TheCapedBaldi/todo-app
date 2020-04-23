18b31137489329827798612f08a710465dfaee9c
Naseebullah Safi <javastastic@gmail.com>
2020-04-23 11:33:00 +0100

    feat: unit test create form

--- src/organisms/Create/Create.test.js
+++ src/organisms/Create/Create.test.js
@@ -1,13 +1,54 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import renderer from "react-test-renderer";
 import "jest-styled-components";
+import { v4 as uuidv4 } from "uuid";
 
 import Create from "./Create";
 
-describe("Create Todo Form", () => {
-  it("should render correctly", () => {
-    const comp = shallow(<Create />);
-    expect(comp.getElements()).toMatchSnapshot();
+describe("<Create />", () => {
+  let wrapper;
+
+  const setState = jest.fn();
+  const onSubmitSpy = jest.fn();
+  const useStateSpy = jest.spyOn(React, "useState");
+
+  useStateSpy.mockImplementation((init) => [init, setState]);
+
+  beforeEach(() => {
+    wrapper = mount(shallow(<Create onSubmit={onSubmitSpy} />).get(0));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render Create", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  describe("Name input", () => {
+    it("should capture name correctly onChange", () => {
+      const name = wrapper.find("#name").at(0);
+      name.instance().value = "Test";
+      name.simulate("change");
+      expect(setState).toHaveBeenCalledWith("Test");
+    });
+  });
+
+  describe("Description input", () => {
+    it("should capture description correctly onChange", () => {
+      const description = wrapper.find("#description").at(0);
+      description.instance().value = "Lorem ipsum";
+      description.simulate("change");
+      expect(setState).toHaveBeenCalledWith("Lorem ipsum");
+    });
+  });
+
+  describe("Form submit", () => {
+    it("should submit form successfully", () => {
+      wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+      expect(onSubmitSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
