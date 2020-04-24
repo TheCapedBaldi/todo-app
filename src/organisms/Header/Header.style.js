03c814ab91889a75e2c1f3f355dc7d48e6ff19c5
Naseebullah Safi <javastastic@gmail.com>
2020-04-24 10:41:31 +0100

    fix: update width of header on mobile view

--- src/organisms/Header/Header.style.js
+++ src/organisms/Header/Header.style.js
@@ -6,12 +6,4 @@ export const StyledHeader = styled.header`
   @media screen and (min-width: 300px) {
     max-width: 300px;
   }
-
-  @media screen and (min-width: 400px) {
-    max-width: 400px;
-  }
-
-  @media screen and (min-width: 600px) {
-    max-width: 600px;
-  }
 `;
