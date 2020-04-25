11f2fc20b81a4153a78107249d35006d1c307ba5
Naseebullah Safi <javastastic@gmail.com>
2020-04-25 19:46:10 +0100

    [test]: add unit tests to todo component

--- src/molecules/Todo/Todo.test.js
+++ src/molecules/Todo/Todo.test.js
@@ -27,8 +27,62 @@ describe("<Todo />", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it("should return initial state of userAction", () => {
-    const { userActions } = reducers(undefined, {});
-    expect(userActions).toEqual({});
+  describe("Redux", () => {
+    it("should return initial state of userAction", () => {
+      const { userActions } = reducers(undefined, {});
+      expect(userActions).toEqual({});
+    });
+
+    it("should add todo to todos reducer", () => {
+      const { todos } = reducers(
+        { todos: {}, userActions: { isRecording: true } },
+        {
+          type: "TODO_ADD",
+          payload: {
+            id: "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b",
+            name: "1",
+            description: "1",
+            date: "Sat Apr 25 2020 16:26:23 GMT+0100 (British Summer Time)",
+          },
+        }
+      );
+      expect(todos).toEqual({
+        "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b": {
+          id: "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b",
+          name: "1",
+          description: "1",
+          date: "Sat Apr 25 2020 16:26:23 GMT+0100 (British Summer Time)",
+        },
+      });
+    });
+
+    it("should add the create action to actions stack", () => {
+      const { userActions } = reducers(
+        {
+          todos: {
+            "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b": {
+              id: "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b",
+              name: "1",
+              description: "1",
+              date: "Sat Apr 25 2020 16:26:23 GMT+0100 (British Summer Time)",
+            },
+          },
+          userActions: { isRecording: true },
+        },
+        {
+          type: "ACTION_ADD_CREATE",
+          payload: {
+            id: "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b",
+            action: "ADD",
+          },
+        }
+      );
+      expect(userActions).toEqual({
+        isRecording: true,
+        actions: [
+          { id: "7b2a3db5-af85-42a5-9440-fa4dc6c7e12b", action: "ADD" },
+        ],
+      });
+    });
   });
 });
